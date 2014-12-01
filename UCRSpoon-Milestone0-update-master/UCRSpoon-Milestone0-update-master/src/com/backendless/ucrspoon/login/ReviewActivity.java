1b48f08fea458f6ba336a286cd44e8e077f82f71
samuel villarreal <protohero1@gmail.com>
2014-11-30 19:20:55 -0800

    Taking pictures or uploading in Review activity
    Those pictures are now saved onto the files of the database. The Picture
    table is filled out and the rest of the review process is unchanged.
    Supports unlimited amount of uploaded pictures at this time.

--- UCRSpoon-Milestone0-update-master/UCRSpoon-Milestone0-update-master/src/com/backendless/ucrspoon/login/ReviewActivity.java
+++ UCRSpoon-Milestone0-update-master/UCRSpoon-Milestone0-update-master/src/com/backendless/ucrspoon/login/ReviewActivity.java
@@ -1,17 +1,33 @@
 package com.backendless.ucrspoon.login;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
+
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.*;
+
 import com.backendless.Backendless;
 import com.backendless.BackendlessCollection;
 import com.backendless.async.callback.AsyncCallback;
+import com.backendless.async.callback.BackendlessCallback;
 import com.backendless.exceptions.BackendlessFault;
+import com.backendless.files.BackendlessFile;
 import com.backendless.persistence.*;
+import com.backendless.ucrspoon.data.Pictures;
 import com.backendless.ucrspoon.data.Restaurant;
 import com.backendless.ucrspoon.data.Dish;
 import com.backendless.ucrspoon.data.Review;
@@ -23,9 +39,15 @@ public class ReviewActivity extends Activity{
 	
 	private Button Submit;
 	private Button up;
+	private Button take;
 	
 	private String dish;
 	private Intent i;
+	
+	private String name;
+	private Pictures bpic;
+	private int flag = 0;
+	
 	
 	
 	public void onCreate( Bundle savedInstanceState)
@@ -50,9 +72,26 @@ public class ReviewActivity extends Activity{
 	      @Override
 	      public void onClick( View view )
 	      {
-			//i = new Intent(view.getContext(), Review2Activity.class);
-	        //onReviewButtonClicked(view);
-			//finish();
+	    	  flag++;
+	    	  Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+	    	  photoPickerIntent.setType("image/*");
+	    	  startActivityForResult(photoPickerIntent,1);
+	    	 
+	    	  
+	      }
+	    } );
+	    take = (Button) findViewById( R.id.takep );
+
+	    take.setOnClickListener( new View.OnClickListener()
+	    {
+	      @Override
+	      public void onClick( View view )
+	      {
+	    	  flag++;
+	    	  Intent photoTake = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+	    	  startActivityForResult(photoTake, 101);
+	    	 
+	    	  
 	      }
 	    } );
 	  
@@ -167,6 +206,106 @@ public class ReviewActivity extends Activity{
 		 
 	}
 
+	
+	 @Override
+	  protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	  {
+		  if(requestCode == 1 || requestCode == 101)
+		  {
+			  if(resultCode == RESULT_OK)
+			  {
+				  //save pic here
+				  Uri pic = data.getData();
+				  //random name for file
+				  name = UUID.randomUUID().toString() + ".jpeg";
+				  //convert file into bitmap
+				  Bitmap bitmap = null;
+				try {
+					bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), pic);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				  try
+				  {
+					  Backendless.Files.Android.upload(bitmap, Bitmap.CompressFormat.JPEG, 100 , name , "Pictures", new AsyncCallback<BackendlessFile>()
+							  {
+						    @Override
+						    public void handleResponse( final BackendlessFile backendlessFile )
+						    {
+						    	//save to table Pictures
+						    	bpic = new Pictures();
+						    	bpic.setFileLocation(name);
+						    	bpic.setUser(Backendless.UserService.CurrentUser().getEmail());
+						    	bpic.setR_id(Integer.toString(getIntent().getIntExtra("sid", 0)));
+						    	
+						    	//prompt for description
+						    	AlertDialog.Builder alert = new AlertDialog.Builder(ReviewActivity.this);
+
+						    	alert.setTitle("Picture Description");
+						    	alert.setMessage("Describe the picture you are uploading");
+
+						    	// Set an EditText view to get user input 
+						    	final EditText input = new EditText(ReviewActivity.this);
+						    	alert.setView(input);
+
+						    	alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+						    	public void onClick(DialogInterface dialog, int whichButton) {
+						    	  String value = input.getText().toString();
+						    	  // Do something with value!
+						    	    bpic.setDescription(value);
+							    	//save now
+							    	bpic.saveAsync( new AsyncCallback<Pictures>()
+											{
+
+												@Override
+												public void handleFault(BackendlessFault arg0) {
+													// TODO Auto-generated method stub
+												}
+
+												@Override
+												public void handleResponse(Pictures arg0) {
+													// TODO Auto-generated method stub
+													  showToast( " Picture added." );
+												}
+				 
+											});
+						    	  }
+						    	});
+
+						    	alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+						    	  public void onClick(DialogInterface dialog, int whichButton) {
+						    	    // Canceled.
+						    		  return;
+						    	  }
+						    	});
+
+						    	alert.show();
+						    
+						    	
+						    }
+
+						    @Override
+						    public void handleFault( BackendlessFault backendlessFault )
+						    {
+						    	showToast(backendlessFault.toString());
+						    }
+						    } );
+				  }
+				  catch(Exception e)
+				  {
+					  showToast("e thrown");
+					  showToast(e.toString());
+				  }
+				  
+				  
+			  }
+		  }
+	  }
+	 
 	private void showToast(String string) {
 		// TODO Auto-generated method stub
 	    Toast.makeText( this, string, Toast.LENGTH_SHORT ).show();
